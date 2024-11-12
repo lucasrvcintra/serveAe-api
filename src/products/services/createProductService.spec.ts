34382b6b7c1d1f3b8f760ccf26cada77bdd1e763
lucasvrcintra <lucasrcintra@hotmail.com>
2024-11-12 20:20:10 -0300

    implementing tests

---
+++ src/products/services/createProductService.spec.ts
@@ -0,0 +1,44 @@
+import { describe, it } from '@jest/globals';
+import { CreateProductService } from './createProductService';
+import { ProductPrismaRepository } from '../repositories/productPrismaRepository';
+
+enum Category {
+  ENTRADA = 'ENTRADA',
+  PRATO_PRINCIPAL = 'PRATO_PRINCIPAL',
+  SOBREMESA = 'SOBREMESA',
+  BEBIDA = 'BEBIDA',
+}
+
+let createProductService: CreateProductService;
+let productPrismaRepository: ProductPrismaRepository;
+
+jest.mock('../repositories/productPrismaRepository');
+describe('CreateProductService', () => {
+  beforeEach(() => {
+    productPrismaRepository = new ProductPrismaRepository();
+    createProductService = new CreateProductService(productPrismaRepository);
+  });
+
+  it('should create a new product', async () => {
+    const newProductData = {
+      name: 'Product 1',
+      description: 'Description 1',
+      price: 10,
+      imageUrl: 'https://example.com/image.jpg',
+      category: Category.PRATO_PRINCIPAL,
+    };
+
+    const newProduct = {
+      id: '37fd1baf-e41b-4cd6-87e7-1347f5db51cb',
+      ...newProductData,
+    };
+
+    productPrismaRepository.create = jest.fn().mockResolvedValue(newProduct);
+
+    const product = await createProductService.create(newProductData);
+
+    expect(product).toEqual(newProduct);
+    expect(product.id).toStrictEqual('37fd1baf-e41b-4cd6-87e7-1347f5db51cb');
+    expect(productPrismaRepository.create).toHaveBeenCalledWith(newProductData);
+  });
+});
