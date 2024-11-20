1fe9dc964f06b9f6d0f146d58c4e4c4d5aa145b5
Lucas Cintra <lucasrcintra63@gmail.com>
2024-11-20 15:05:21 -0300

    validating create user schema

--- src/users/dto/createUserDto.ts
+++ src/users/dto/createUserDto.ts
@@ -1,10 +1,28 @@
 import { z } from 'zod';
 
 export const CreateUserSchema = z.object({
-  name: z.string().min(3, 'Nome inválido'),
-  email: z.string().email('Email inválido'),
+  name: z
+    .string()
+    .min(5, { message: 'Deve ter no mínimo 5 caracteres' })
+    .max(30, { message: 'Deve ter no máximo 30 caracteres' })
+    .regex(/^(?!.*<\/?[^>]+>)[^'\\]+$/, {
+      message: 'Caracteres inválidos',
+    }),
+  email: z
+    .string()
+    .min(5, { message: 'Deve ter no mínimo 5 caracteres' })
+    .email({ message: 'Formato inválido.' })
+    .regex(/^(?!.*<\/?[^>]+>)[^'\\]+$/, {
+      message: 'Caracteres inválidos',
+    }),
   address: z.string().min(5, 'Endereço inválido'),
-  phone: z.string().min(11, 'Telefone inválido'),
+  phone: z
+    .string()
+    .min(9, { message: 'Deve ter no mínimo 9 caracteres' })
+    .max(14, { message: 'Deve ter no máximo 14 caracteres' })
+    .regex(/^\d{9,14}$/, {
+      message: 'Formato inválido. Deve conter apenas números.',
+    }),
 });
 
 export type CreateUserDto = z.infer<typeof CreateUserSchema>;
